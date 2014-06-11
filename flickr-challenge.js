3fd42f98972fede1fe016ec69d69d5beb3e73c3a
Carlo De Gregorio <cdegregorio@gmail.com>
2014-06-11 14:56:04 -0700

    Finished! Loads photos as you scroll to the bottom

--- flickr-challenge.js
+++ flickr-challenge.js
@@ -4,20 +4,30 @@ $( document ).ready(function() {
 var counter = 1;
 var url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=38ebaada8366e437d637ddfb76023f44&page=" + 
   counter + "&format=json";
-
+var maxPages = 1;
 
 var requestFromFlickr = function(url) {
 $.ajax(url, { dataType: 'jsonp', jsonp: 'jsoncallback' })
 .then(function(data, status, xhr) {
+  counter += 1;
+  maxPages = data.photos.pages;
+  
   displayPhotos(data, status, xhr); }, function(xhr, status, error){
   console.log('You have created some error Now');
   console.log(error);
 });
-
 };
+
+var getDocHeight = function() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+};
+
 var displayPhotos = function (data, status, xhr) {
-  console.log(status);
-  console.log(data.photos.photo[0]);
 
   var currentRow = data.photos.photo;
 
@@ -26,35 +36,44 @@ var displayPhotos = function (data, status, xhr) {
     var nextDiv = $('<div/>', {
       'class': 'photo_square'
     });
-    console.log('Creating the number ' + index + ' photo div');
-
+    
     $(nextDiv).appendTo('div.container');
-    console.log('Appending #'+ index + ' to container');
 
     var imgUrl = 'https://farm' + photo.farm +
       '.staticflickr.com/' + photo.server +
       '/' + photo.id + '_' + photo.secret +
       '.jpg';
-    console.log('Building #' + index + ' image url');
 
     var nextImg =  $('<img>', { src : imgUrl,
       alt: "View on Flickr",
       size: "100%"});
-    console.log('Adding #' + index + ' img tag');
 
     $(nextImg).appendTo('div.photo_square:last-child');
-    console.log('Appending #' + index + ' to its div');
 
     $(nextImg).wrap($('<a></a>', {href: imgUrl}));
-    console.log('Wrapping #' + index + ' with an a tag');
-
+    url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=38ebaada8366e437d637ddfb76023f44&page=" + 
+    counter + "&format=json";
   });
 
 
 };
 
+var checkScrolling = function () {$(window).scroll(function() {
+       if($(window).scrollTop() + $(window).height() === getDocHeight()) {
+        if (counter <= maxPages) {
+          console.log(counter, maxPages);
+          alert('botom');
+          requestFromFlickr(url);
+        }
+        else {
+       alert('Nothing new to see here');}
+   }
+
+});};
+
 requestFromFlickr(url);
+checkScrolling();
+
 
 });
-
 
